d552dcf9c51ad0aef352556058a3f01484911cc4
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-06-21 16:06:08 +0530

    simple http server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abhishekkushwahaa/go-server
+
+go 1.22.1
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Welcome to Go-Server!");
+	fileServer := http.FileServer(http.Dir("./client"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler) 
+	http.HandleFunc("/hello", helloHandler)
+	
+	fmt.Printf("Server is statring at Port 8080!\n")
+
+	if err := http.ListenAndServe(":8080", nil); 
+	err != nil {
+		log.Fatal(err);
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "Page Not Found!", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not accepted!", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello from Golang Server!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() Err: %v", err)
+		return
+	}
+	fmt.Fprint(w, "Post requested sent successfully!\n")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Name: %s\n", name);
+	fmt.Fprintf(w, "Email: %s\n", email);
 }
